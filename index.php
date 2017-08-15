0830351b7492619cf896dec8631786b673db7ce4
Fred Scott <fscott@southeast.edu>
2017-08-15 12:08:20 -0500

    inspirational quotes

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<?php
+
+//1525 Program 3
+
+//The best way to get a project done faster is to start sooner. -Jim Highsmith
+
+//The first 90 percent of the code accounts for the first 90 percent of the development time. The remaining 10 percent of the code accounts for the other 90 percent of the development time. -Tom Cargill
+
+echo 'go get it';
+?>
