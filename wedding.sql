5f2b0683fe4cda339f2b1c415b4a05652eeb66bc
lwetzel900 <35115239+lwetzel900@users.noreply.github.com>
2018-02-25 20:08:43 -0600

    added sql to wedding.sql to create and use database when imported

--- wedding.sql
+++ wedding.sql
@@ -21,7 +21,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `wedding`
 --
-
+CREATE DATABASE IF NOT EXISTS `wedding` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `wedding`;
 -- --------------------------------------------------------
 
 --
